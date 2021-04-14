bbe98546bb34ba55c456a7c5526a9837bf1f1278
Brittany Owen <owen.brittany@gmail.com>
2021-04-14 17:39:49 -0500

    cleared warnings

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ChevronsRight } from "react-feather";
-import {Link} from "react-router-dom"
+// import { ChevronsRight } from "react-feather";
+// import {Link} from "react-router-dom"
 
 function Home(props) {
   return (
